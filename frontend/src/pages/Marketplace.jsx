b38465585c8ae057413400ced89f39161ddaeeb2
Shenal <rwind384@gmail.com>
2026-04-17 22:36:50 +0530

    did still back to home button

--- frontend/src/pages/Marketplace.jsx
+++ frontend/src/pages/Marketplace.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { ArrowLeftIcon, FilterIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Marketplace = () => {
+  const navigate = useNavigate()
+  const [showFilterphone, setShowFilterphone] = React.useState(false)
   return (
-    <div>
-      <h1>Marketplace Page</h1>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+      <div className='flex items-center justify-between text-slate-500'>
+        <button onClick={()=>{navigate('/'); scrollTo(0, 0)}}
+          className='flex items-center gap-2 py-5'>
+          <ArrowLeftIcon className="size-4" />
+          Back to Home
+        </button>
+        <button onclick={()=>setShowFilterphone(true)} className='flex sm:hidden items-center gap-2 py-5'>
+          <FilterIcon className='size-4'/>
+           Filters
+          
+          </button>
+      </div>
+      <div></div>
     </div>
   )
 }
