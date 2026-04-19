03072c56c1088418c419462839c4c7455fe767f3
Shane Silva <shanesilva212@gmail.com>
2026-04-19 16:06:13 +0530

    updated ListingDetails.jsk

--- frontend/src/pages/ListingDetails.jsx
+++ frontend/src/pages/ListingDetails.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const ListingDetails = () => {
+
+  const navigate = useNavigate()
+  const currency = import.meta.env.VITE_CURRENCY || 'රු';
+
+  const [listing, setListing] = useState(null)
+  
+  const {listingId} = useParams()
+  const {listings} = useSelector((state)=>state.listings)
+
+  useState(() => {
+    const listing = listings.find((listing) => listing.id === listingId)
+    if(listing){
+      setListing(listing)
+    }
+  }, [listingId, listings])
+
   return (
     <div>
-      <h1>ListingDetails Page</h1>
+      
     </div>
   )
 }
